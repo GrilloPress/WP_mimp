47c976cb8861e4fab24ae1ad0ad80b92bfea08a8
GrilloPress <grillopress@gmail.com>
2015-09-11 11:14:39 +0000

    create inital mimp theme

--- wordpress/wp-content/themes/sth/footer.php
+++ wordpress/wp-content/themes/sth/footer.php
@@ -11,10 +11,10 @@
 
     </div><!-- #content -->
 
-    <footer id="colophon" class="site-footer" role="contentinfo">
-      <div class="site-info container">
-        <div class="row">
-          <div id="widget-footer">
+    <footer id="colophon" role="contentinfo">
+      <div class="site-footer">
+        <div class="container">
+          <div id="widget-footer" class="row">
 		        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
 		        <?php endif; ?>
 		        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
@@ -25,13 +25,16 @@
 		        <?php endif; ?>
 		      </div>
         </div>
-        
-        <div class="row">
-          <div class="col-md-12">
-            <p>&copy; Sheffield Teaching Hospitals NHS Foundation Trust</p>
+     </div><!-- .site-info -->
+      
+        <div class="sub-footer">
+          <div class="container">
+            <div class="col-md-12">
+              <p>&copy; Sheffield Teaching Hospitals NHS Foundation Trust</p>
+            </div>
           </div>
         </div>
-      </div><!-- .site-info -->
+     
     </footer><!-- #colophon -->
   </div><!-- #page -->
 
