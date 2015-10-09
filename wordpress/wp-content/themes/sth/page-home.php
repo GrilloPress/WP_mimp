8500d73808aa1608ad18fb3421ee31593f001cd1
GrilloPress <grillopress@gmail.com>
2015-10-09 11:00:34 +0000

    update sass block style and url links

--- wordpress/wp-content/themes/sth/page-home.php
+++ wordpress/wp-content/themes/sth/page-home.php
@@ -30,7 +30,7 @@ Template Name: Homepage
               <div class="row">
                   <div class="col-md-3 col-sm-6 col-xs-12">
                     <a href="<?php echo home_url( '/patient-information/' ); ?>">
-                      <img src="<?php echo content_url('/themes/wp-mimp/images/radiology-reception.jpg'); ?>" class="hidden-xs" width="100%">
+                      <img src="<?php echo get_template_directory_uri() . '/images/patient-partnership.jpg' ;?>" class="hidden-xs" width="100%">
                       <div class="well" id="">
                         <h2>Patient Information</h2>
                         <footer>Learn more about your patient journey and our services</footer>
@@ -40,7 +40,7 @@ Template Name: Homepage
 
                   <div class="col-md-3 col-sm-6 col-xs-12">
                     <a href="<?php echo home_url( '/departments-and-services/' ); ?>">
-                      <img src="<?php echo content_url('/themes/wp-mimp/images/services-stock.jpg'); ?>" class="hidden-xs" width="100%">
+                      <img src="<?php echo get_template_directory_uri() . '/images/services.jpg' ;?>" class="hidden-xs" width="100%">
                       <div class="well" id="">
                         <h2>A-Z of Services</h2>
                         <footer>Explore our commercial and patient services</footer>
@@ -50,7 +50,7 @@ Template Name: Homepage
 
                   <div class="col-md-3 col-sm-6 col-xs-12">
                     <a href="#" class="hidden-xs" width="100%">
-                      <img src="<?php echo content_url('/themes/wp-mimp/images/commercial.jpg'); ?>" class="hidden-xs" width="100%">
+                      <img src="<?php echo get_template_directory_uri() . '/images/commerical-web.jpg' ;?>" class="hidden-xs" width="100%">
                       <div class="well" id="">
                         <h2>Commercial Services</h2>
                         <footer>Find out more about our world-class commerical services</footer>
@@ -60,7 +60,7 @@ Template Name: Homepage
 
                   <div class="col-md-3 col-sm-6 col-xs-12">
                     <a href="<?php echo home_url( '/referral-information/' ); ?>">
-                      <img src="<?php echo content_url('/themes/wp-mimp/images/doctor.jpg'); ?>" class="hidden-xs" width="100%">
+                      <img src="<?php echo get_template_directory_uri() . '/images/gps.jpg' ;?>" class="hidden-xs" width="100%">
                       <div class="well" id="">
                         <h2>Referral Information</h2>
                         <footer>Find out how to make a referral to our service</footer>
