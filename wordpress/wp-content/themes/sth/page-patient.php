24abb1f78646c4230ac0fd6a71733f657c4ca34c
GrilloPress <grillopress@gmail.com>
2015-10-13 08:57:26 +0000

    update p-lead class

--- wordpress/wp-content/themes/sth/page-patient.php
+++ wordpress/wp-content/themes/sth/page-patient.php
@@ -18,11 +18,21 @@ get_header(); ?>
     <div class="row">
       <main id="main" class="col-md-8" role="main">
         
-        <?php $the_query = new WP_Query( array( 'category_name' => 'Patient Information' ) ) ;?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class('well'); ?>>
+          <header class="entry-header">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+          </header><!-- .entry-header -->
         
-        <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-            
-        <?php $parent = $post->post_parent;?>
+        <section>
+          <p class="lead">At Sheffield Teaching Hospitals we aim to provide you with the best patient experience possible.</p>
+          <p>The Medical Imaging and Medical Physics departments work in all of the major adult hospitals in Sheffield such as the Northern General, Royal Hallamshire, Jessop Wing Hospital, Weston Park Hospital and the Charles Clifford Dental Hospital</p>
+          <p>The vast majority of your questions will be answered on the relevant department or service page, or contained within a service’s patient leaflet. If you cannot find the answer you are looking for on a service’s page, the best course of action is to call the department or service and ask.&nbsp;Failing that, get in contact with our Patient Service team who will try to help you where they can.</p>
+        </section>
+        
+        <?php 
+        $the_query = new WP_Query( array( 'category_name' => 'Patient Information' ) );
+        if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();
+        $parent = $post->post_parent;?>
         
         <div class="well">
           <h2><a href="<?php echo get_permalink( ); ?>"><?php echo get_the_title($parent) ;?></a></h2>
@@ -34,7 +44,7 @@ get_header(); ?>
           <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
         <?php endif; ?>
 
-
+        </article>
 		  </main><!-- #main -->
       
       <aside class="col-md-4">
